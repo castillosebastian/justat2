a37233cdd27fe03243abb44a89f0b940309fe95d
sebacastillo <castilloclaudiosebastian@gmail.com>
2020-12-16 18:31:44 -0300

    primeros pasos

---
+++ prueba1.py
@@ -0,0 +1,24 @@
+from random import choice
+
+
+class Person:
+
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return 'Hello {name}'.format(name=self.name)
+
+
+def main():
+    people = [
+        Person('Jane'),
+        Person('Bob'),
+        Person('Julia')
+    ]
+    person = choice(people)
+    print(person)
+
+
+if __name__ == '__main__':
+    main()
